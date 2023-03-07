6bd45c6e328b1896fa40cb7d64f0d777cebe10fc
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-03-07 06:46:26 -0500

    loop js lab.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+function writeCards(name, value) {
+
+    let cardMessage = []
+    for (let i = 0; i < name.length; i++) {
+        cardMessage.push(`Thank you, ${name[i]}, for the wonderful ${value} gift!`);
+
+    }
+
+    return cardMessage;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown() {
+    let countDown = 11;
+    while (countDown--) {
+        console.log(countDown);
+
+    }
+}
